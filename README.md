3a94feafd66574bb32bc89451927c6ed00854b1c
Stephan Schmitz <stephan.schmitz@sunzinet.com>
2019-03-13 09:30:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# _Nicegist_ - a permanent gist.io alternative
+
+> writing for hackers · zero setup · publish in seconds
+
+**A pure JavaScript gist.io replacement, hosted on GitHub***.
+
+_* As long as GitHub exists, it will not die. Yay!_
+
+## About
+
+When I recognized that [gist.io](https://github.com/idan/gistio) is dead, I stumbled upon [a comment](https://github.com/idan/gistio/issues/74#issuecomment-348884248) in gist.io's issue tracker, that suggested to build a pure JavaScript implementation to be hosted on GitHub pages.
+
+I liked the idea. Thus, _Nicegist_ was born.
+
+## Features
+
+- 💥 Supports gist embedding (use `<gist>` tags, i.e. `<gist>file.sh</gist>`)
+- Supports public and secret gists
+- Supports CommonMark / GFM syntax
+- Automatic code block syntax highlighting
+- Automatic headline anchors
+
+## Usage
+
+1. Create a gist on Github with one or more Markdown-syntax files.
+2. Note the gist ID. (It's usually a longish alpha-numeric string like `dab5cf7977008e504213`.)
+3. View your writing presented nicely at `nicegist.github.io/YOUR_GIST_ID`
+
+**Bookmarklet:**
+
+Be lazy and drag the [Nicegist bookmarklet](https://gist.githubusercontent.com/eyecatchup/7442b083383908d7c925981ff082fea7/raw/7bcb67f301d71a0c1435629822845a4ac12605a0/nicegist-bookmarklet.js) to your bookmarks bar.  
+Click it when you’re on a gist page, and it will take you to the corresponding Nicegist page.
+
+```js
+javascript:(function(){if(location.hostname==="gist.github.com"){var gistId=location.pathname.split("/").pop();if(location.pathname.split("/").length>2&&gistId.length)location.href="https://eyecatchup.github.io/nicegist/?id="+gistId}})();
+```
+
+**Examples:**
+
+- Code blocks: [Nicegist](https://eyecatchup.github.io/nicegist/?id=2f35faad4d4fa55810422283f7bc3b78), [Source](https://gist.github.com/eyecatchup/2f35faad4d4fa55810422283f7bc3b78)
+- Tables: [Nicegist](https://eyecatchup.github.io/nicegist/?id=79b95b862ca276c0748c9bab90a758e0), [Source](https://gist.github.com/eyecatchup/79b95b862ca276c0748c9bab90a758e0)
+- 💥 Embedded gists: [Nicegist](https://eyecatchup.github.io/nicegist/?id=3382937), [Source](https://gist.github.com/surma/3382937)
+- Text formatting, images & more: [Nicegist](https://eyecatchup.github.io/nicegist/?id=96e67c2dd38419b200f9efcd56c2e8e3), [Source](https://gist.github.com/eyecatchup/96e67c2dd38419b200f9efcd56c2e8e3)
+- Example writeup: [Nicegist](https://eyecatchup.github.io/nicegist/?id=dab5cf7977008e504213), [Source](https://gist.github.com/eyecatchup/dab5cf7977008e504213)
+
+## Under the hood
+
+- [GitHub Gist API](https://developer.github.com/v3/gists/#get-a-single-gist) for fetching gists
+- [Chromium destilled webpage layout](https://chromium.googlesource.com/chromium/src/+/refs/heads/master/components/dom_distiller/) for optimized reading experience
+- [markdown-it](https://github.com/markdown-it/markdown-it) for Markdown parsing
+- [markdown-it-anchor](https://github.com/valeriangalliat/markdown-it-anchor) for automated header anchors
+- [highlight.js](https://highlightjs.org/) for code block syntax highlighting
+- Some regex sugar and a hidden gist feature for gist embedding
+- [GitHub Pages SPA hack](http://www.backalleycoder.com/2016/05/13/sghpa-the-single-page-app-hack-for-github-pages/) for pretty URLs
+
+## License
+
+(c) 2019, Stephan Schmitz <eyecatchup@gmail.com>  
+License: MIT, <http://eyecatchup.mit-license.org>  
+URL: <https://nicegist.github.io/>  
---
+++ static/app.js
@@ -0,0 +1,258 @@
+(function(window) {
+    var GithubApi = {
+        xhr: null,
+        apiBaseUrl: 'https://api.github.com',
+        getXMLHttpRequest: function () {
+            if (!!window.XMLHttpRequest) {
+                this.xhr = new window.XMLHttpRequest;
+            }
+            else {
+                try {
+                    this.xhr = new ActiveXObject("MSXML2.XMLHTTP.3.0");
+                } catch (e) {
+                    this.xhr = null;
+                }
+            }
+
+            if (!this.xhr) {
+                window.console.log("AJAX (XMLHTTP) not supported by your client.");
+            }
+        },
+        get: function (endpoint, success, failure) {
+            this.getXMLHttpRequest();
+
+            var self = this.xhr,
+                requestUrl = this.apiBaseUrl + endpoint;
+
+            this.xhr.open('GET', requestUrl, true);
+            this.xhr.setRequestHeader('Accept', 'application/json');
+
+            self.onload = function() {
+                if (self.status >= 200 && self.status < 400) {
+                    window.console.log('Successfully called ' + requestUrl);
+                    try {
+                        var json = JSON.parse(self.responseText);
+                    } catch (e) {
+                        window.console.log('Error parsing response as JSON. Returning raw response data.');
+                    }
+
+                    var response = !json ? self.responseText : json;
+
+                    success(response);
+                }
+                else {
+                    window.console.log('Error requesting ' + requestUrl +
+                        '. Response Status-Code is ' + self.status);
+                    failure();
+                }
+            }
+            self.onerror = function() {
+                window.console.log('There was an error (of some sort) connecting to ' + requestUrl);
+                failure();
+            };
+
+            this.xhr.send();
+        },
+        getGist: function(gistId, success, failure) {
+            var url = '/gists/' + gistId;
+            this.get(url, success, failure);
+        }
+    };
+
+    window.GithubApi = GithubApi;
+})(window);
+
+var externalLinks = function() {
+    for(var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+        var b = c[a];
+        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank");
+    }
+};
+
+var hideLoadingIndicator = function() {
+    document.querySelector('#loadingIndicator').style.display = 'none';
+};
+
+var showFooter = function(elemId) {
+    document.querySelector('#' + elemId).style.display = 'block';
+};
+
+var parseQueryString = (function(pairList) {
+    var pairs = {};
+    for (var i = 0; i < pairList.length; ++i) {
+        var keyValue = pairList[i].split('=', 2);
+        if (keyValue.length == 1) {
+            pairs[keyValue[0]] = '';
+        } else {
+            pairs[keyValue[0]] = decodeURIComponent(keyValue[1].replace(/\+/g, ' '));
+        }
+    }
+    return pairs;
+})(window.location.search.substr(1).split('&'));
+
+// Since we can not access the iframe to get its scroll height (cross origin),
+// we calculate the height by counting the lines in the embedded gist.
+// Ugly, but works reliable.
+var getIframeHeight = function(filename) {
+    for (var n in files.others) {
+        if (files.others[n].filename === filename) {
+            var matches = files.others[n].content.match(/\n/g);
+            var lines = ((matches && matches.length) ? matches.length : 0) + 1;
+            // 22px = line height in embedded gists (with .pibb extension)
+            // 40px = embedded gists footer height
+            // 3px = cumulated border height for embedded gists
+            // 8px = body margin for embedded gists
+            return (lines * 22) + 40 + 3 + 8;
+        }
+    }
+    return false;
+};
+
+// smooth anchor scrolling
+var smoothScrollTo = function(elem) {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: elem.getBoundingClientRect().top + window.scrollY
+    });
+};
+// not-so-smooth anchor scrolling for IE
+var jankyScrollTo = function(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        jankyScrollTo(element, to, duration - 10);
+    }, 10);
+};
+
+var loadGist = function(gistId) {
+    GithubApi.getGist(gistId, function(gist) {
+        if (gist) {
+            console.dir(gist);
+            hideLoadingIndicator();
+
+            if (gist.id && gist.id.length) {
+                // use gist description as a document title / headline
+                if (gist.description.length) {
+                    $titleHolder.textContent = gist.description;
+                    document.title = gist.description;
+                } else {
+                    $titleHolder.textContent = 'Untitled document';
+                }
+
+                // get all markdown files to be parsed
+                for (var n in gist.files) {
+                    if (gist.files[n].language === 'Markdown') {
+                        files.markdown.push(gist.files[n]);
+                    } else {
+                        files.others.push(gist.files[n]);
+                    }
+                }
+
+                // parse markdown files
+                if (files.markdown.length) {
+                    var html = '';
+                    var md = window.markdownit({linkify: true});
+                    md.use(window.markdownItAnchor, {
+                        level: 1,
+                        permalink: true,
+                        permalinkClass: 'header-anchor',
+                        permalinkSymbol: '¶',
+                        permalinkBefore: true
+                    });
+
+                    for (var i in files.markdown) {
+                        html += md.render(files.markdown[i].content);
+                    }
+
+                    // do we need to embed other gists?
+                    var matches = html.match(/&lt;gist&gt;(.*?)&lt;\/gist&gt;/gi);
+                    if (matches && matches.length) {
+                        for (var x in matches) {
+                            var filename = matches[x].replace('&lt;gist&gt;', '').replace('&lt;/gist&gt;', '');
+                            var h = getIframeHeight(filename);
+                            if (h !== false) {
+                                html = html.replace(matches[x], '<iframe class="embedded-gist" style="height:' + h + 'px" src="https://gist.github.com/' + gistId + '.pibb?file=' + filename + '" scrolling="no"></iframe>');
+                            }
+                        }
+                    }
+
+                    // write gist content
+                    $contentHolder.innerHTML = html;
+
+                    // add link to gist comment section, if we have comments
+                    if (gist.comments > 0) {
+                        document.querySelector('#gistComments').innerHTML = '<a target="_blank" href="https://gist.github.com/' + gistId + '#comments">' + gist.comments + ' comments</a>';
+                    }
+
+                    // add syntax highlighting to code blocks
+                    var codeBlocks = document.querySelectorAll('pre');
+                    for (var c in codeBlocks) {
+                        try {
+                            hljs.highlightBlock(codeBlocks[c]);
+                        } catch(e) {}
+                    }
+
+                    // open external links in new tab
+                    externalLinks();
+
+                    // smooth-scroll to anchor
+                    if (location.hash.length) {
+                        setTimeout(function() {
+                            var elem = document.getElementById(location.hash.substring(1));
+                            var isIE = /Trident|MSIE/.test(navigator.userAgent);
+                            if (!isIE) {
+                                smoothScrollTo(elem);
+                            } else {
+                                var root = document.documentElement || document.body;
+                                jankyScrollTo(root, elem.offsetTop, 600);
+                            }
+                        }, 200);
+                    }
+                } else {
+                    $contentHolder.textContent = 'No markdown files attached to gist ' + gistId;
+                }
+            }
+        }
+    }, function(error) {
+        console.warn(error);
+        hideLoadingIndicator();
+        $titleHolder.textContent = 'Error fetching gist.'
+    });
+};
+
+var init = function(gistId) {
+    if (typeof gistId === 'undefined' || gistId === '') {
+        loadGist('7442b083383908d7c925981ff082fea7');
+        showFooter('footerIntro');
+    } else {
+        loadGist(gistId);
+        showFooter('footerPost');
+    }
+};
+
+var $titleHolder = document.querySelector('#titleHolder'),
+    $contentHolder = document.querySelector('#gistContent'),
+    gistId = '',
+    files = {
+        markdown: [],
+        others: []
+    };
+
+(function() {
+    var redirect = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+
+    if (redirect && redirect !== location.href) {
+        history.replaceState(null, null, redirect);
+        gistId = redirect.split('/').pop(); // redirected via 404 page hack
+    } else {
+        gistId = parseQueryString['id']; // direct entry
+    }
+
+    init(gistId);
+})();
